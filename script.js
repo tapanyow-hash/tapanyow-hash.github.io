6c81bd6092b24f31342fef943b19fdee999a9cc2
tapanyow-hash <tapanyo.w@gmail.com>
2025-08-14 16:19:02 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered: ', registration.scope);
+      })
+      .catch(err => {
+        console.log('Service Worker registration failed: ', err);
+      });
+  });
+}
+
+
 // Game state variables
 let currentProblem = 1;
 const totalProblems = 15;
